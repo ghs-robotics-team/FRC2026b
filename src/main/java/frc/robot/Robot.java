4aa512c9582ae1e0a0ea46037db8083fbe589693
GraffitiDS <driverstation@graffiti.io>
2026-03-14 09:50:26 -0500

    Added FMS to smartdashboard.

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,7 +7,9 @@ package frc.robot;
 import com.ctre.phoenix6.HootAutoReplay;
 
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
 
@@ -31,6 +33,16 @@ public class Robot extends TimedRobot {
     public void robotPeriodic() {
         m_timeAndJoystickReplay.update();
         CommandScheduler.getInstance().run();
+
+        // Update match score and status to SmartDashboard
+        double matchTimeSeconds = DriverStation.getMatchTime();
+        int minutes = (int) matchTimeSeconds / 60;
+        int seconds = (int) matchTimeSeconds % 60;
+        String matchTimeFormatted = String.format("%02d:%02d", minutes, seconds);
+        SmartDashboard.putString("Match Time", matchTimeFormatted);
+        
+        SmartDashboard.putBoolean("Is Enabled", DriverStation.isEnabled());
+        SmartDashboard.putString("Mode", DriverStation.isAutonomous() ? "Autonomous" : "Teleop");
 
         /*
          * This example of adding Limelight is very simple and may not be sufficient for on-field use.
