88d5bf846c258cc2b3e78dcbd8e0e5c76a23f522
GraffitiDS <driverstation@graffiti.io>
2026-03-26 18:18:27 -0500

    Bug Fixing

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.ConditionalCommand;
 import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -169,12 +170,12 @@ public class RobotContainer {
     private final HoodAnglerPositionCommand hoodAngleHighPosition = new HoodAnglerPositionCommand(hoodAngler, -100);
 
     // Intaking / Outtaking
-    private final IntakeOnlyCommand intakeOnlyCommand = new IntakeOnlyCommand(intake, 0.8);
+    private final IntakeOnlyCommand intakeOnlyCommand = new IntakeOnlyCommand(intake, 0.7);
     private final SequentialCommandGroup intakeRampDown = new IntakeOnlyCommand(intake, 0.25).withTimeout(0.25).andThen(
         new IntakeOnlyCommand(intake, 0.1).withTimeout(0.25)
     );
     
-    private final IntakeOnlyCommand outtakeOnlyCommand = new IntakeOnlyCommand(intake, -0.8);
+    private final IntakeOnlyCommand outtakeOnlyCommand = new IntakeOnlyCommand(intake, -0.7);
     private final SequentialCommandGroup outtakeRampDown = new IntakeOnlyCommand(intake, -0.25).withTimeout(0.25).andThen(
         new IntakeOnlyCommand(intake, -0.1).withTimeout(0.25)
     );
